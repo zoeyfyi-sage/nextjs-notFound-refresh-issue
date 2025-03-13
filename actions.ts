3dc78c1025aa615f3e238d4c1ac7082c7b0e02bd
Zoey Sheffield <Zoey.Sheffield@sage.com>
2025-03-13 12:47:42 +0000

    default to true

--- actions.ts
+++ actions.ts
@@ -1,10 +1,10 @@
 "use server"
 
-let toggle: boolean = false;
+let toggle: boolean = true;
 
 export async function setToggle(value: boolean) {
     console.log(toggle, value)
-  toggle = value;
+    toggle = value;
 }
 
 export async function getToggle() {
